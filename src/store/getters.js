7749fde2cc88469fe30777cf0ca6d47f652ef8c0
王文国 <924958592@qq.com>
2021-06-03 14:00:53 +0800

    人资项目初始化

---
+++ src/store/getters.js
@@ -0,0 +1,7 @@
+/* eslint-disable indent */
+const getters = {
+        sidebar: state => state.app.sidebar,
+        device: state => state.app.device
+    }
+    // eslint-disable-next-line eol-last
+export default getters
---
+++ src/store/modules/user.js
@@ -0,0 +1,8 @@
+/* eslint-disable indent */
+/* eslint-disable eol-last */
+export default {
+    namespaced: true,
+    state: {},
+    mutations: {},
+    actions: {}
+}
